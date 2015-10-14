4e37fd39dbfc8c4ee114d623c04ce1ec47e00939
tobyealden <toby.ealden@gmail.com>
2015-10-14 15:09:42 +0100

    refactor ddp xrh connection

--- index.js
+++ index.js
@@ -2,52 +2,40 @@
  * Created by toby on 13/10/15.
  */
 
-var config = require("./config.json");
+var _config = require("./config.json");
+var _xrhAccessToken;
 
 var fileServer = require("./fileServer");
 fileServer.start();
 
-var DDPClient = require("ddp");
-var _ddpclient = new DDPClient({
-    host : config.xrhServer,
-    port : config.xrhPort,
-    ssl  : config.ssl || false,
-    autoReconnect : true,
-    autoReconnectTimer : config.autoReconnectTimer || 5000,
-    maintainCollections : true,
-    ddpVersion : '1',
-    useSockJs: false
-  });
-
-var _connected = false;
-_ddpclient.connect(function(error, reconnect) {
-  if (error) {
-    console.log("DDP connection error: " + error.toString());
-  } else {
-    _connected = true;
-    if (reconnect) {
-      console.log("DDP re-connected");
-    }
-    console.log("DDP connected");
-    _ddpclient.call("/app/auth", [config.capCredentials], function(err, result) {
-      console.log("callback");
+var xrhConnection = require("./xrhConnection");
+xrhConnection.start(_config, function(err, reconnect) {
+  if (!err) {
+    xrhConnection.authenticate(function(err, result) {
       if (err) {
-        console.log("/app/auth error %s", err.message);
+        console.log("xhr connection auth error %s", err.message);
       } else {
-        console.log("/app/auth result: %s", JSON.stringify(result));
-        _ddpclient.subscribe("datasets",[{accessToken: result.id}],function() {
-          console.log("%j",_ddpclient.collections.Dataset);
-        });
+        console.log("xhr connection auth result ", result);
+        _xrhAccessToken = result.id;
+        if (!reconnect) {
+          xrhConnection.subscribe(_xrhAccessToken, "datasets", {}, function(err) {
+            if (err) { console.log(err); }
+          });
+          xrhConnection.observe("Dataset", function(id) {
+            console.log("got dataset %s",id);
+            console.log("content is ", xrhConnection.collection("Dataset")[id]);
+          });
+        }
       }
-    }, function() {
-      console.log("updated callback");
-    });
+    })
+  } else {
+    console.log("xhr connection failed");
   }
 });
 
 setTimeout(function() {
   var exec = require('child_process').exec;
-  exec('node -v', function(error, stdout, stderr) {
+  exec('node -v', {shell:"/system/bin/sh"}, function(error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
---
+++ xrhConnection.js
@@ -0,0 +1,112 @@
+/**
+ * Created by toby on 14/10/15.
+ */
+  
+module.exports = (function() {
+  "use strict";
+  var _config;
+  var _accessToken;
+  var _ddpClient;
+  var _connected = false;
+  
+  var ddpInitialise = function(config, onConnect) {
+    var DDPClient = require("ddp");
+    if (_ddpClient) return;
+    
+    _config = config;
+    
+    _ddpClient = new DDPClient({
+      host : _config.xrhServer,
+      port : _config.xrhPort,
+      ssl  : _config.ssl || false,
+      autoReconnect : true,
+      autoReconnectTimer : _config.autoReconnectTimer || 5000,
+      maintainCollections : true,
+      ddpVersion : '1',
+      useSockJs: false
+    });
+
+    _ddpClient.connect(function(err, reconnect) {
+      if (err) {
+        console.log("DDP connection error: " + err.toString());
+        _connected = false;
+        onConnect(err);
+      } else {
+        _connected = true;
+        _accessToken = "";
+        onConnect(null,reconnect);
+        if (reconnect) {
+          console.log("DDP re-connected");
+        }
+        console.log("DDP connected");
+      }
+    });
+  
+    _ddpClient.on("socket-close", function() {
+      _connected = false;
+    });
+  
+    _ddpClient.on("socket-error", function(err) {
+      console.log("socket error: %s",err.message);
+      _connected = false;
+    });     
+  };
+  
+  var ddpAuthenticate = function(cb) {
+    if (!_connected) {
+      console.log("not connected");
+    }
+    _ddpClient.call("/app/auth", [_config.capCredentials], function(err, result) {
+      console.log("callback");
+      cb(err, result);
+    });
+  };
+  
+  var ddpSubscribe = function(accessToken, publication, params, cb) {
+    if (!_connected) {
+      console.log("ddpSubscribe - not connected");
+      process.nextTick(function() { cb(new Error("not connected")); });
+    }
+    params.accessToken = accessToken;
+    _ddpClient.subscribe(publication, [params], cb);
+  };
+  
+  var ddpObserve = function(collection, added, changed, removed) {
+    var observer = _ddpClient.observe(collection);
+    observer.added = function(id, doc) {
+      console.log("[ADDED] to " + observer.name + ":  " + id);
+      if (added) {
+        added(id);
+      }
+    };
+    observer.changed = function(id, oldFields, clearedFields, newFields) {
+      console.log("[CHANGED] in " + observer.name + ":  " + id);
+      console.log("[CHANGED] old field values: ", oldFields);
+      console.log("[CHANGED] cleared fields: ", clearedFields);
+      console.log("[CHANGED] new fields: ", newFields);
+      if (changed) {
+        changed(id, oldFields, clearedFields, newFields);
+      }
+    };
+    observer.removed = function(id, oldValue) {
+      console.log("[REMOVED] in " + observer.name + ":  " + id);
+      console.log("[REMOVED] previous value: ", oldValue);
+      if (removed) {
+        removed(id, oldValue);
+      }
+    };
+  };
+  
+  var getCollection = function(name) {
+    return _ddpClient.collections[name];
+  };
+  
+  return {
+    start: ddpInitialise,
+    authenticate: ddpAuthenticate,
+    subscribe: ddpSubscribe,
+    observe: ddpObserve,
+    collection: getCollection
+  }
+}());
+
